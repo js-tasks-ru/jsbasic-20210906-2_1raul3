22fbd1de7f31de91f37f2597e3e610fbf968690a
Error0013 <1raul3@gmail.com>
2021-09-13 15:23:20 +0300

    описание того, что вы сделали

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -11,7 +11,45 @@ function print(text) {
  */
 function isValid(name) {
   // ваш код...
+  if (name == null) {
+    return false;
+  } else if (name.length < 4) {
+    return false;
+  } else if (name.includes(' ')){
+    return false;
+  }
+    else {
+  return true;
+  }
+
+  // if (name == null) {
+  //   return false
+  // }
+  // nameTest = name
+  // if (nameTest == "")  {
+  //   return false;
+  // } else {
+  //   return true;
+  // }
+
+  // if (!name) {
+  //   return false
+  // }
+  // if (name !== " ") {
+  //   return true
+  // } 
+
+  // if (name == null || name.lenght < 4) {
+  //   return false
+  // } else if (name == ""){
+  //   return false
+  // }
+  //   else {
+  //   return true
+  // }
+
 }
+
 
 function sayHello() {
   let userName = prompt('Введите ваше имя');
