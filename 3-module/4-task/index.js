b4f99735dc2b94525b6bf901f3e6a4c86395e49e
Error0013 <1raul3@gmail.com>
2021-09-21 22:50:31 +0300

    module 3

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,6 @@
 function showSalary(users, age) {
   // ваш код...
+  const filteredMenus = users.filter((user) => user.age <= age);
+  let filteredStr = filteredMenus.map((user) => user.name + ', ' + user.balance);
+  return filteredStr.join('\n');
 }
