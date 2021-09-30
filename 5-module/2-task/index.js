07fc5f3853db89a442a9ce83201146c22e99eb7c
Error0013 <1raul3@gmail.com>
2021-09-30 17:45:19 +0300

    module 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,14 @@
 function toggleText() {
   // ваш код...
+  let button = document.querySelector('.toggle-text-button');
+  let text = document.querySelector('#text');
+  let onclick = () => {
+    if (text.hidden == true) {
+      text.hidden = false;
+    }
+    else {
+      text.hidden = true;
+    }
+  };
+  button.addEventListener('click', onclick);
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,36 @@
 function initCarousel() {
   // ваш код...
+  let carouselRight = document.querySelector('.carousel__arrow_right');
+  let carouselLeft = document.querySelector('.carousel__arrow_left');
+  let carouselInner = document.querySelector('.carousel__inner');
+  let carouselSlide = document.querySelectorAll('.carousel__slide');
+  let counter = 0;
+
+  setArrow();
+
+  carouselRight.addEventListener('click', () => {
+    counter++;
+    carouselInner.style.transform = `translateX(-${counter * carouselInner.offsetWidth}px)`;
+    setArrow();
+  });
+
+  carouselLeft.addEventListener('click', () => {
+    counter--;
+    carouselInner.style.transform = `translateX(-${counter * carouselInner.offsetWidth}px)`;
+    setArrow();
+  });
+
+  function setArrow(){
+    if (counter === 0) {
+      carouselLeft.style.display = 'none';
+    } else {
+      carouselLeft.style.display = '';
+    }
+  
+    if (counter >= carouselSlide.length -1) {
+      carouselRight.style.display = 'none';
+    } else {
+      carouselRight.style.display = '';
+    }
+  }
 }
